30e2963b3b55dc17b6d1106608f7ea2735fcdf16
Rohan Gaonkar <gaonkar70@gmail.com>
2020-11-22 14:38:59 +0530

    version 3

--- index.js
+++ index.js
@@ -39,9 +39,9 @@ app.use("/", require('./Routes/index'));
 app.use("/user", require('./Routes/user'));
 
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static('../build'));
+  app.use(express.static('client/build'));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname,'../','build','index.html'))
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
   })
 
 }
